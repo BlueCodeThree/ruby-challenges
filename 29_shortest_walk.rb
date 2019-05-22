632ce16de8b6c06f4952df3c323c0bbf5c96b4cb
BlueCodeThree <carliemhamilton@gmail.com>
2019-05-22 16:51:47 +1000

    29 done

--- 29_shortest_walk.rb
+++ 29_shortest_walk.rb
@@ -2,10 +2,10 @@
 # Difficulty: hard
 
 # A man was given directions to go from one point to another.
-# The directions were "NORTH", "SOUTH", "WEST", "EAST". 
-# Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too. 
-# Going to one direction and coming back the opposite direction is a needless effort. 
-# Since this is the wild west, with dreadfull weather and not much water, 
+# The directions were "NORTH", "SOUTH", "WEST", "EAST".
+# Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
+# Going to one direction and coming back the opposite direction is a needless effort.
+# Since this is the wild west, with dreadfull weather and not much water,
 # it's important to save yourself some energy, otherwise you might die of thirst!
 
 # How I crossed the desert the smart way.
@@ -13,28 +13,28 @@
 
 # ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"].
 
-# You can immediatly see that going "NORTH" and then "SOUTH" is not reasonable, 
-# better stay to the same place! So the task is to give to the man a simplified 
+# You can immediatly see that going "NORTH" and then "SOUTH" is not reasonable,
+# better stay to the same place! So the task is to give to the man a simplified
 # version of the plan. A better plan in this case is simply:
 
 # ["WEST"]
 
 # Other examples:
-# In ["NORTH", "SOUTH", "EAST", "WEST"], 
-# the direction "NORTH" + "SOUTH" is going north and coming back right away. 
+# In ["NORTH", "SOUTH", "EAST", "WEST"],
+# the direction "NORTH" + "SOUTH" is going north and coming back right away.
 # What a waste of time! Better to do nothing.
 
-# The path becomes ["EAST", "WEST"], now "EAST" and "WEST" annihilate each other, 
-# therefore, the final result is [] 
+# The path becomes ["EAST", "WEST"], now "EAST" and "WEST" annihilate each other,
+# therefore, the final result is []
 
-# In ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"], 
-# "NORTH" and "SOUTH" are not directly opposite but they become directly 
-# opposite after the reduction of "EAST" and "WEST" so the whole path is reducible 
+# In ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"],
+# "NORTH" and "SOUTH" are not directly opposite but they become directly
+# opposite after the reduction of "EAST" and "WEST" so the whole path is reducible
 # to ["WEST", "WEST"].
 
 # Task
-# Write a function shortest_walk which will take an array of strings and 
-# returns an array of strings with the needless directions removed 
+# Write a function shortest_walk which will take an array of strings and
+# returns an array of strings with the needless directions removed
 # (W<->E or S<->N side by side).
 
 # Examples
@@ -43,6 +43,42 @@
 
 # The test with the challenge use rspec
 
+# 22 May 2019
 def shortest_walk(walk)
-    # your code goes here
+    walk.each do |direction|
+        case direction
+        when 'NORTH'
+            walk.each do |x|
+                if x == 'SOUTH'
+                    walk.delete_at(walk.index('NORTH'))
+                    walk.delete_at(walk.index('SOUTH'))
+                end
+            end
+        when 'SOUTH'
+            walk.each do |x|
+                if x == 'NORTH'
+                    walk.delete_at(walk.index('NORTH'))
+                    walk.delete_at(walk.index('SOUTH'))
+                end
+            end
+        when 'EAST'
+            walk.each do |x|
+                if x == 'WEST'
+                    walk.delete_at(walk.index('WEST'))
+                    walk.delete_at(walk.index('EAST'))
+                end
+            end
+        when 'WEST'
+            walk.each do |x|
+                if x == 'EAST'
+                    walk.delete_at(walk.index('WEST'))
+                    walk.delete_at(walk.index('EAST'))
+                end
+            end
+        end
+    end
+    if walk.length > 1
+        shortest_walk(walk)
+    end
+    return walk
 end
