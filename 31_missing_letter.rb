00c5d7e427f1f130d1c9f7d1563196980d58c361
BlueCodeThree <carliemhamilton@gmail.com>
2019-05-22 20:07:42 +1000

    31 done!

--- 31_missing_letter.rb
+++ 31_missing_letter.rb
@@ -1,10 +1,10 @@
 # Missing letter
 # Difficulty: medium/hard
 
-# Write a method that takes an array of consecutive (increasing) 
+# Write a method that takes an array of consecutive (increasing)
 # letters as input and that returns the missing letter in the array.
 
-# You will always get an valid array. And it will be always exactly 
+# You will always get an valid array. And it will be always exactly
 # one letter be missing. The length of the array will always be at least 2.
 # The array will always contain letters in only one case.
 
@@ -15,6 +15,20 @@
 
 # Included test uses rspec
 
+# 22 May 2019
 def missing_letter(word)
-    # your code goes here
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for letter in alphabet.split("")
+        if letter == word[0]
+            start = alphabet.index(letter)
+            break
+        end
+    end
+
+    for letter in word
+        if letter != alphabet[start]
+            return alphabet[start]
+        else start +=1
+        end
+    end
 end
