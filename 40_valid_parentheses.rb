508a34e8d727a025af2cad1407fcbb2beed7f346
BlueCodeThree <carliemhamilton@gmail.com>
2019-06-07 15:04:31 +1000

    40 done!

--- 40_valid_parentheses.rb
+++ 40_valid_parentheses.rb
@@ -1,7 +1,7 @@
 # Valid parentheses
 
 # Write a method that returns true if the argument string contains valid parentheses
-# Examples that evaluate to true: 
+# Examples that evaluate to true:
 # valid_parentheses?("()") => true
 # valid_parentheses?("abc") => true
 # valid_parentheses?("(a(bc))") => true
@@ -11,6 +11,14 @@
 # valid_parentheses?("abc)") => false
 # valid_parentheses?("(a(bc)") => false
 
+# 7 June 2019
 def valid_parentheses?(string)
-    # your code here
+    brackets = Hash.new(0)
+    string.chars.each do |char|
+        if char == "(" || char == ")"
+            brackets[char] += 1
+        end
+        return false if brackets[")"] > brackets["("]
+    end
+    return brackets["("] == brackets[")"] ? true : false
 end
